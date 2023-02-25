53047f1bc1d54e7da9180a862556809a94c2eba9
Misslittle87 <92373716+Misslittle87@users.noreply.github.com>
2023-02-25 23:01:00 +0100

    Style mainpage, tabbar
    Added style to mainpage, and made a tabbar.

---
+++ Model/MainModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stallapp.Model
+{
+    public class MainModel
+    {
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+    }
+}
--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Stallapp.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Stallapp.ViewModel
 {
-    internal class MainViewModel : BaseViewModel
+    public partial class MainViewModel : ObservableObject
     {
+        [ObservableProperty]
+        DateTime currentDate = DateTime.Now;
     }
 }
